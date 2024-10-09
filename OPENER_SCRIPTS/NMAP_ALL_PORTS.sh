caf2809bb78203503eb08d1863b7f9892732ac58
jrd3n <jordon_services@hotmail.com>
2024-10-09 09:01:04 +0100

    removed -sV to speed things up

--- OPENER_SCRIPTS/NMAP_ALL_PORTS.sh
+++ OPENER_SCRIPTS/NMAP_ALL_PORTS.sh
@@ -59,7 +59,7 @@ if [ -f "$file_name.xml" ]; then
 
         # nmap $fetcher_ip -p- -Pn -sV --stats-every=5s --max-retries 1000 --min-rate 50 -T2 --host-timeout 60s -oX $file_name.xml
 
-        sudo nmap $fetcher_ip -p 0-65535 -sS -sU -Pn -sV -oA $file_name
+        sudo nmap $fetcher_ip -p 0-65535 -sS -sU -Pn -oA $file_name
 
         # Convert XML to HTML
         xsltproc $file_name.xml -o $file_name.html
@@ -72,7 +72,7 @@ else
 
     # Run Nmap scan and save XML output
 
-    sudo nmap $fetcher_ip -p 0-65535 -sS -sU -Pn -sV --stats-every=5s --max-parallelism 255 -T3 --min-rate=3000 -oA $file_name
+    sudo nmap $fetcher_ip -p 0-65535 -sS -sU -Pn--stats-every=5s --max-parallelism 255 -T3 --min-rate=3000 -oA $file_name
 
     # Convert XML to HTML
     xsltproc $file_name.xml -o $file_name.html
